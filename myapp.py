9edf72bf970e606b60d3a0e419719d23cdbbeca0
ahmedelnaqa <72502706+ahmedelnaqa@users.noreply.github.com>
2023-11-28 20:56:57 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,9 +1,13 @@
+from flask import Flask, render_template
 import streamlit as st
-from flask import Flask
 
 app = Flask(__name__)
-@app.route('/api/data')
-def hello_world():
-    return 'hello testkkk'
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/streamlit')
+def streamlit():
+    st.set_page_config(page_title="My Streamlit App")
+    st.write("Hello, world!")
 if __name__ == '__main__':
     app.run()
