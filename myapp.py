c0b3479cd851dad1cb7fde6a630c10a2b8f89cef
ahmedelnaqa <72502706+ahmedelnaqa@users.noreply.github.com>
2023-11-28 20:24:13 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-from flask import Flask
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'hello testkkk'
-if __name__ == '__main__':
-    app.run()
+st.write("Helloo Samir")
+st.write("تمام")
