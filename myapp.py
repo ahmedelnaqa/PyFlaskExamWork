4ce1cd15a176bf0b20a7c2f7cad676adc361edef
ahmedelnaqa <72502706+ahmedelnaqa@users.noreply.github.com>
2023-11-28 21:12:36 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template
 import streamlit as st
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/streamlit')
-def streamlit():
-    st.set_page_config(page_title="My Streamlit App")
-    st.write("Hello, world!")
-if __name__ == '__main__':
-    app.run()
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
+
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
+
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/foo')
+    def serve_foo():
+        return 'This page is served via Flask!'
+
+    app.run(port=8888)
+
+
+# We'll never reach this part of the code the first time this file executes!
+
+# Your normal Streamlit app goes here:
+x = st.slider('Pick a number')
+st.write('You picked:', x)
